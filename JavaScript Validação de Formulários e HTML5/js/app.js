a79b1d048937678e71a5efa6b7fd431955457ddf
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-03-22 11:03:16 -0300

    mascara de valores

--- JavaScript Validação de Formulários e HTML5/js/app.js
+++ JavaScript Validação de Formulários e HTML5/js/app.js
@@ -1,8 +1,24 @@
-import { validate } from './validacao.js';
+import {
+    validate
+} from './validacao.js';
 
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
+
+    if (input.dataset.tipo == 'preco') {
+        input = SimpleMaskMoney.setMask(input, {
+            allowNegative: false,
+            prefix: 'R$ ',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'
+        });
+    }
+
+
     input.addEventListener('blur', (event) => {
         validate(event.target);
     })
